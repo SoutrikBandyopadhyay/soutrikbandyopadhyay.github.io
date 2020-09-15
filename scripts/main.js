b78c2a5f474a0cc0286d45f9962d435616b70b22
SoutrikBandyopadhyay <soutrik.band@gmail.com>
2020-09-15 13:58:51 +0530

    Vue Stuff

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,56 @@
-// const quoteList = [
-//     "Be yourself; everyone else is already taken.",
-//     "So many books, so little time"
-// ];
+var TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
 
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
 
-var pubList = [{
-        "name": "Meow",
-        "author": "Oscar Wilde"
-
-    },
-    {
-        "name": "Meow2",
-        "author": "Oscar Wilde2"
-
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
     }
-];
 
-// var numQuotes = quoteList.length;
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-// var quoteObj = quoteList[Math.floor(Math.random() * numQuotes)];
+    var that = this;
+    var delta = 200 - Math.random() * 100;
 
+    if (this.isDeleting) { delta /= 2; }
 
-Vue.component('publication', {
-    props: ['article'],
-    template: `
-    <span>
-        <h2> {{article.name}} </h2>
-        <p> {{article.author}} </p>
-    </span>
-    `
-})
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
 
-var app = new Vue({
-    el: '#app',
-    component: ['publication'],
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
 
-    data: {
-        pubs: pubList,
-
-    },
-
-})
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
